cd060ea81bb869aea4fd936ea6a1a5747781bcd9
Yash Jadav <jadavyash603@gmail.com>
2023-07-30 17:51:37 +0530

    final version

--- index.js
+++ index.js
@@ -28,7 +28,9 @@ app.use("/uploads", express.static("uploads"));
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({ origin: "http://localhost:3000", withCredentials: true }));
+app.use(
+  cors({ origin: "https://forza-customs.vercel.app/", withCredentials: true })
+);
 
 // Routes
 app.use("/api/users", userRoutes);
